dda42f57b16a9aa8378d4b31759d472f0d9b841e
RNL <41996199+RNLgit@users.noreply.github.com>
2022-05-15 16:56:01 +1000

    change to ver 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="rpictrl",
-    version="0.0.1",
+    version="0.1.0",
     description='RPI cpu temperature pwm fan controller',
     long_description=open('README.md').read(),
     classifiers=[],
